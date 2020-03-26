158c7fc9bca262a890ec6c5a535cfb47212b48f8
Yun <wangyun@lieluobo.com>
2020-03-26 14:45:35 +0800

    update spm

--- Package.swift
+++ Package.swift
@@ -9,18 +9,11 @@ let package = Package(
         .iOS(.v10),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "FSPagerView",
-            targets: ["FSPagerView"]),
+        .library(name: "FSPagerView",targets: ["FSPagerView"])
     ],
-
-    /// 依赖那些第三方
-    dependencies: [],
     targets: [
         .target(
             name: "FSPagerView",
-            dependencies:[],
-            path: "FSPageViewExample-Swift"),
+            path: "Sources"),
     ]
 )
